78248f3d21f39be393588e18895831e251fd88ea
kiri3914 <t.bektur.2002@gmail.com>
2023-04-05 10:42:22 +0600

    templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import EmployeeForm
+from .models import Employee
 
-# Create your views here.
+
+def add_new(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/')
+            except:
+                pass
+    else:
+        form = EmployeeForm()
+    return render(request, 'index.html', {'form': form})
+
+
+def index(request):
+    employees = Employee.objects.all()
+    return render(request, 'show.html', {'employees': employees})
+
