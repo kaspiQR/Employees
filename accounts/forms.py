6576177806bf90eb83cebba3d8fa28443dc73d25
kiri3914 <t.bektur.2002@gmail.com>
2023-04-08 21:39:50 +0600

    add accounts

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,25 +1,30 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
-    first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
-    last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
+    username = forms.CharField(max_length=50,
+                               required=True,
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(max_length=50,
+                             required=True,
+                             help_text='Обязательное поле. Введите почту',
+                             widget=forms.EmailInput(attrs={'class': 'form-control',
+                                                            'placeholder': 'Email address'}))
+    first_name = forms.CharField(max_length=30, required=True,
+                                 widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=30, required=True,
+                                widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    password1 = forms.CharField(max_length=55, required=True,
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(max_length=55, required=True,
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
-
-        widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
-        }
 
 
 class LoginForm(forms.ModelForm):
